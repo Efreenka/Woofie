360e9e9edb53263efdaafa9181249fa7151dc366
Efreenka <kaja.bilkova@seznam.cz>
2023-08-21 10:37:59 +0200

    routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,9 +4,20 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/dog',
-      name: 'dog',
-      component: () => import('../views/DogView.vue')
+      path: '/',
+      component: () => import('../views/layout/DefaultLayout.vue'),
+      children: [
+        {
+          path: 'dog',
+
+          component: () => import('../views/DogView.vue')
+        },
+        {
+          path: 'breed',
+
+          component: () => import('../views/BreedView.vue')
+        }
+      ]
     }
   ]
 })
